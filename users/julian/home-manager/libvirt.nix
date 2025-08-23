c5b510b9a28a41de1cdbf32be5c3f854f57b74b4
julianpoyourow <julian@poyourow.com>
2025-08-23 20:12:56 +0000

    wip

--- users/julian/home-manager/libvirt.nix
+++ users/julian/home-manager/libvirt.nix
@@ -3,7 +3,7 @@
   config,
   ...
 }: {
-  home-manager.users.julian.dconf.settings = lib.mkIf (config.virtualisation.libvirtd.enable) {
+  home-manager.users.julian.dconf.settings = config.lib.mkIf (config.virtualisation.libvirtd.enable) {
     "org/virt-manager/virt-manager/connections" = {
       autoconnect = ["qemu:///system"];
       uris = ["qemu:///system"];
