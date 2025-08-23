b84e9bcc7dd62193992724f6dd9c6c28882ea9b8
julianpoyourow <julian@poyourow.com>
2025-08-23 05:33:54 +0000

    wip

--- hosts/coop-erinyes/default.nix
+++ hosts/coop-erinyes/default.nix
@@ -11,6 +11,7 @@ specialArgs.nixpkgs-2505.lib.nixosSystem {
 
     (import "${specialArgs.home-manager-2505}/nixos")
     (import ../../users/coop {stateVersion = "25.05";})
+    (import ../../users/coop-admin {stateVersion = "25.05";})
 
     ../../mixins/base
     ../../mixins/steam
