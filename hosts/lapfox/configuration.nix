d2766baa9173aa5f79785161aa6e221b10009ad3
Boushi <chris.gomez.1993@gmail.com>
2025-09-11 23:33:27 -0700

    Make bluetooth a mixin

--- hosts/lapfox/configuration.nix
+++ hosts/lapfox/configuration.nix
@@ -5,7 +5,6 @@
   ...
 }: {
   networking.hostName = "lapfox";
-  hardware.bluetooth.enable = true;
 
   system.stateVersion = "25.05";
 }
--- hosts/lapfox/default.nix
+++ hosts/lapfox/default.nix
@@ -16,5 +16,6 @@ specialArgs.nixpkgs-2505.lib.nixosSystem {
     ../../mixins/shared/base
     ../../mixins/shared/boush
 
+    ../../mixins/system/bluetooth.nix
   ];
 }
