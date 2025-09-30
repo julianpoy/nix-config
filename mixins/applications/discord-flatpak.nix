e6e6617dc56c84b5490d27440d9c784ffb686803
Julian Poyourow <julian@poyourow.com>
2025-09-29 19:59:42 -0700

    chore: update discord-flatpak to 111

--- mixins/applications/discord-flatpak.nix
+++ mixins/applications/discord-flatpak.nix
@@ -12,7 +12,7 @@
       appId = "com.discordapp.Discord";
       origin = "flathub";
       # Use `flatpak remote-info --log flathub com.discordapp.Discord | less` to find latest commit.
-      commit = "a90dab4f7695db564cb4fd5b518aa38dc0490eb77a6b5fb8077f27fe7dc1f4f3";
+      commit = "afb63b7e9eedbffd75dbacc2d043a5a87c6cec95151e6db28b43ce40f27d8533";
     }
   ];
 
