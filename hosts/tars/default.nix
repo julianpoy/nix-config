fb13c7bfc22ce2f71df24d75bd5496ccfd0f31a6
Julian Poyourow <julian@poyourow.com>
2025-10-03 14:52:40 -0700

    feat: fwupd for tars

--- hosts/tars/default.nix
+++ hosts/tars/default.nix
@@ -39,6 +39,7 @@ specialArgs.nixpkgs-2505.lib.nixosSystem {
     ../../mixins/system/amdtuning.nix
     ../../mixins/system/appimage.nix
     ../../mixins/system/flatpak.nix
+    ../../mixins/system/fwupd.nix
     ../../mixins/system/latest-kernel.nix
   ];
 }
