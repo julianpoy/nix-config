4a4b96f205de2547892b1dd76403641b54efac64
Julian Poyourow <julian@poyourow.com>
2025-10-22 21:29:34 -0700

    test: add dotnet-runtime

--- mixins/system/dotnet-sdk.nix
+++ mixins/system/dotnet-sdk.nix
@@ -17,22 +17,20 @@
 
   programs.nix-ld.libraries = with pkgs; [
     glibc
-    gcc.cc.lib  # if C++ runtime needed
+    gcc.cc.lib
     fontconfig
     xorg.libX11
     xorg.libICE
     xorg.libSM
     stdenv.cc.cc
     libcap
-  ] ++ (with pkgs.pkgsi686Linux; [
-    glibc
-    gcc.cc.lib  # if C++ runtime needed
-    fontconfig
-    xorg.libX11
-    xorg.libICE
-    xorg.libSM
-    libcap
-  ]);
+
+    # Add 32-bit runtime support
+    pkgsi686Linux.glibc
+    pkgsi686Linux.gcc.cc.lib
+    pkgsi686Linux.xorg.libX11
+    pkgsi686Linux.xorg.libXext
+  ];
 
   # environment.variables = {
   #   NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
