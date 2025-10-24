528ce8f96c55731ae30f9ca1ccfb47dc708668f9
Julian Poyourow <julian@poyourow.com>
2025-10-24 13:55:23 -0700

    update unit

--- users/home-manager-mixins/steam-perf.nix
+++ users/home-manager-mixins/steam-perf.nix
@@ -5,33 +5,40 @@
   ...
 }: {
   systemd.user.startServices = true;
-  systemd.user.services.steam-dev-flags = {
-    description = "Configure Steam developer flags for download behavior";
-    wantedBy = [ "default.target" ];
-
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
+  systemd.user.services.steam-performance-dev-flags = {
+    Unit = {
+      Description = "Configure Steam developer flags for improved performance";
+      After = [ "graphical-session.target" ];
     };
 
-    script = ''
-      STEAM_DIR="$HOME/.steam/steam"
-      CFG="$STEAM_DIR/steam_dev.cfg"
-      THREADS=$(${pkgs.coreutils}/bin/nproc)
-      STEAM_THREADS=$((THREADS - 4))
+    Service = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = ''
+        ${pkgs.bash}/bin/bash -c '
+          STEAM_DIR="$HOME/.steam/steam"
+          CFG="$STEAM_DIR/steam_dev.cfg"
+          THREADS=$(${pkgs.coreutils}/bin/nproc)
+          STEAM_THREADS=$((THREADS - 4))
 
-      # Ensure minimum of 4 threads
-      if [ $STEAM_THREADS -lt 4 ]; then
-        STEAM_THREADS=4
-      fi
+          # Ensure minimum of 4 threads
+          if [ $STEAM_THREADS -lt 4 ]; then
+            STEAM_THREADS=4
+          fi
 
-      if [ -e "$STEAM_DIR" ]; then
-        cat > "$CFG" <<EOF
+          if [ -e "$STEAM_DIR" ]; then
+            cat > "$CFG" <<EOF
 @nClientDownloadEnableHTTP2PlatformLinux 0
 @fDownloadRateImprovementToAddAnotherConnection 1.1
 unShaderBackgroundProcessingThreads $STEAM_THREADS
 EOF
-      fi
-    '';
+          fi
+        '
+      '';
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
   };
 }
