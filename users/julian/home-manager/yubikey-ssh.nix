0c894873b7035c1f7fa1a518c2e00ad3f9d3aba0
Julian Poyourow <julian@poyourow.com>
2025-10-24 14:44:33 -0700

    feat: add SSH_AUTH_SOCK session variable

--- users/julian/home-manager/yubikey-ssh.nix
+++ users/julian/home-manager/yubikey-ssh.nix
@@ -46,6 +46,10 @@
     # };
   };
 
+  home.sessionVariables = {
+    SSH_AUTH_SOCK = "$(gpgconf --list-dirs agent-ssh-socket)";
+  };
+
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
