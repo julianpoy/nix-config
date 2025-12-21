f2c235b54980b419c3ef9c9b617ae18d9341201c
Julian Poyourow <julian@poyourow.com>
2025-12-21 12:02:22 -0800

    feat: elysium

--- hosts/elysium/configuration.nix
+++ hosts/elysium/configuration.nix
@@ -6,5 +6,13 @@
 }: {
   networking.hostName = "elysium";
 
+  networking.useDHCP = false;
+  networking.bridges = {
+    br0 = {
+      interfaces = [ "enp14s0" ];
+    };
+  };
+  networking.interfaces.br0.useDHCP = true;
+
   system.stateVersion = "25.05";
 }
