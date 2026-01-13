4bd3e372b576c83914c15f09764253a55c006cc5
Chris Meyer <chrismeyer175@gmail.com>
2026-01-12 21:57:35 -0800

    Update git.nix for chris

--- users/chris/home-manager/git.nix
+++ users/chris/home-manager/git.nix
@@ -7,5 +7,12 @@
   programs.git = {
     userName = "Chris Meyer";
     userEmail = "chrismeyer175@gmail.com";
+
+    extraConfig = {
+      commit.gpgsign = true;
+      tag.gpgSign = true;
+      user.signingkey = "0x72DC5D65A37D62C2";
+      gpg.format = "openpgp";
+    };
   };
 }
